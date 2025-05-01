d861b294b06a39af6c453bb5fdb280b26b2f49f4
antoniochavez18 <antoniochavez18@gmail.com>
2025-05-01 01:02:00 -0400

    main del fernando

---
+++ fdo_main.py
@@ -0,0 +1,49 @@
+#!python
+from pathlib import Path
+
+from main import (
+    ajustar_ganancias,
+    calcular_sensibilidad_cortafuegos,
+    crear_combustibles,
+    crear_opciones_cortafuegos,
+    optimizar_modelo,
+    quemar_soluciones,
+    rodales_con_cortafuegos,
+    simular_crecimiento,
+)
+
+ashape = Path("fdo/instance1_growth_attributes.shp")
+assert ashape.exists()
+
+# 1
+gdf, rodales = simular_crecimiento(area_estudio=ashape, id="fid", mid="growth_mid", outfile="bosque_data.csv")
+
+# 2
+crear_opciones_cortafuegos(gdf, rodales)
+
+# 3
+cortafuegos = calcular_sensibilidad_cortafuegos()
+
+# 4
+rodales_cf, gdf_cf = rodales_con_cortafuegos(
+    rodales,
+    cortafuegos,
+    area_estudio=ashape,
+    area_con_cortafuegos=str(Path("cortafuegos/data_cortafuegos/data_modificada/proto_mod.shp")),
+)
+
+# 5
+valores_objetivo, valores_objetivo_cf, soluciones, soluciones_cf, prices = optimizar_modelo(rodales, rodales_cf)
+
+# paso 6: se crean los combustibles y se guardan en la carpeta de soluciones
+filter, filtro_cf = crear_combustibles(rodales, rodales_cf, soluciones, soluciones_cf, gdf, gdf_cf)
+
+# paso 7: se simula incendios y se adapta el burn probability para acumular informacion de años pasados
+bp_sin_cortafuegos, bp_con_cortafuegos = quemar_soluciones(
+    rodales, rodales_cf, gdf, gdf_cf, filter, filtro_cf, cortafuegos, str(ashape)
+)
+
+# paso 8 y 9: se adapta ganancias y se elije 1 de las 10 soluciones
+biomass_for_solution, biomass_for_solution_con_cortafuegos, vt_sin_cortafuegos, vt_con_cortafuegos = ajustar_ganancias(
+    filter, filtro_cf, bp_sin_cortafuegos, bp_con_cortafuegos, prices
+)
